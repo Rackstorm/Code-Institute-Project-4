0a55729c6bee656bdf0e03b5314d4061e31ec4d8
Rackstorm Nesti <paulinen@live.se>
2023-10-18 19:05:12 +0000

    Adding heroku url to allowed hosts

--- recipeexchange/settings.py
+++ recipeexchange/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'recipeexchange-2021.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'code-project-4-7bed3136eda4.herokuapp.com']
 
 
 # Application definition
