a65059b6708dcdfd376b0f01bb1f15ff35a696dd
Rackstorm Nesti <paulinen@live.se>
2023-10-20 07:08:27 +0000

    Adding categories to the admin panel

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,16 +1,20 @@
 from django.contrib import admin
-from .models import Post, Comment
+from .models import Post, Comment, Category
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-
-    list_display = ('title', 'slug', 'status', 'created_on')
-    search_fields = ['title', 'content']
+    list_display = ('title', 'slug', 'status', 'created_on', 'get_category_title')
+    search_fields = ['title', 'content', 'category__title']
     list_filter = ('status', 'created_on')
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
+
+    def get_category_title(self, obj):
+        return obj.category.title if obj.category else ''
+    get_category_title.short_description = 'Category'
+
 
 
 @admin.register(Comment)
@@ -22,3 +26,9 @@ class CommentAdmin(admin.ModelAdmin):
 
     def approve_comments(self, request, queryset):
         queryset.update(approved=True)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    search_fields = ('title',)
--- recipes/models.py
+++ recipes/models.py
@@ -6,20 +6,25 @@ from cloudinary.models import CloudinaryField
 STATUS = ((0, "Draft"), (1, "Published"))
 
 
+class Category(models.Model):
+    title = models.CharField(max_length=200, unique=True, verbose_name="Category")
+
+    def __str__(self):
+        return self.title
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="recipe_posts"
-    )
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="recipe_posts")
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name="posts")
     featured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    likes = models.ManyToManyField(
-        User, related_name='post_like', blank=True)
+    likes = models.ManyToManyField(User, related_name='post_like', blank=True)
 
     class Meta:
         ordering = ["-created_on"]
