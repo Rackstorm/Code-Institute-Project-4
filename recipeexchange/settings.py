90d59f21a87e1e8cfb4a80b97ef549171b89a943
Rackstorm Nesti <paulinen@live.se>
2023-10-25 08:46:14 +0000

    Chaning settings in order to do testing

--- recipeexchange/settings.py
+++ recipeexchange/settings.py
@@ -102,14 +102,14 @@ WSGI_APPLICATION = 'recipeexchange.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 # DATABASES = {
-#     'default': {
+#      'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+#      }
+#  }
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
