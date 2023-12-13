ac4949c7cb622fa1b2390846088648d708f08abc
Rackstorm Nesti <paulinen@live.se>
2023-12-13 00:09:37 +0000

    Adding urls for creating posts

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,14 +1,12 @@
 from . import views
 from django.urls import path
-from .views import CustomSignupView
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('search/', views.PostSearch.as_view(), name='post_search'),
     path('profile/', views.ProfileView, name='profile'),
+    path('profile/edit/', views.edit_profile, name='edit_profile'),
+    path('create/', views.PostCreateView, name='post_create'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('<slug:slug>/like/', views.PostLike.as_view(), name='post_like'),
-    path('create/', views.PostCreate.as_view(), name='post_create'),
-    path('signup/', CustomSignupView.as_view(), name='account_signup'),
-
 ]
