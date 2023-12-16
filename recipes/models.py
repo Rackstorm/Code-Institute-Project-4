2d8352fcc5c437c027abb1ae62589c7249300055
Rackstorm Nesti <paulinen@live.se>
2023-12-16 19:42:47 +0000

    Solving bugs

--- recipes/models.py
+++ recipes/models.py
@@ -6,15 +6,35 @@ from cloudinary.models import CloudinaryField
 # Choices for the status field in the Post model
 STATUS = ((0, "Draft"), (1, "Published"))
 
-# Model representing a category for posts
 class Category(models.Model):
+    """
+    Model representing a category for posts.
+
+    Attributes:
+        title (str): The title of the category.
+    """
     title = models.CharField(max_length=200, unique=True, verbose_name="Category")
 
     def __str__(self):
         return self.title
 
-# Model representing a blog post
 class Post(models.Model):
+    """
+    Model representing a blog post.
+
+    Attributes:
+        title (str): The title of the post.
+        slug (AutoSlugField): The slug for the post.
+        author (User): The author of the post.
+        category (Category): The category to which the post belongs.
+        featured_image (CloudinaryField): The featured image for the post.
+        excerpt (str): A brief excerpt of the post.
+        updated_on (DateTimeField): The last update timestamp.
+        content (str): The content of the post.
+        created_on (DateTimeField): The creation timestamp.
+        status (int): The status of the post (Draft or Published).
+        likes (ManyToManyField): Users who liked the post.
+    """
     title = models.CharField(max_length=200, unique=True)
     slug = AutoSlugField(populate_from='title', unique=True, always_update=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="recipe_posts")
@@ -39,13 +59,29 @@ class Post(models.Model):
     def delete_post(self):
         self.delete()
 
-# Model representing a like for a specific post by a user
 class PostLike(models.Model):
+    """
+    Model representing a like for a specific post by a user.
+
+    Attributes:
+        post (Post): The post that is liked.
+        user (User): The user who liked the post.
+    """
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-# Model representing a comment on a blog post
 class Comment(models.Model):
+    """
+    Model representing a comment on a blog post.
+
+    Attributes:
+        post (Post): The post to which the comment belongs.
+        name (str): The name of the commenter.
+        email (EmailField): The email of the commenter.
+        body (str): The content of the comment.
+        created_on (DateTimeField): The creation timestamp.
+        approved (bool): Indicates whether the comment is approved or not.
+    """
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     name = models.CharField(max_length=80)
     email = models.EmailField()
@@ -59,8 +95,16 @@ class Comment(models.Model):
     def __str__(self):
         return f"Comment {self.body} by {self.name}"
 
-# Model representing a user profile
 class Profile(models.Model):
+    """
+    Model representing a user profile.
+
+    Attributes:
+        user (User): The user to whom the profile belongs.
+        bio (str): A biography or description of the user.
+        profile_picture (CloudinaryField): The profile picture of the user.
+        liked_posts (ManyToManyField): Posts liked by the user.
+    """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(blank=True)
     profile_picture = CloudinaryField('image', blank=True, null=True)
