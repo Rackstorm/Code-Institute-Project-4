c79f2d6ed230ed001df6ea4366a90116f81b2898
Rackstorm Nesti <paulinen@live.se>
2023-12-17 00:05:07 +0000

    Solving bugs

--- recipes/signals.py
+++ recipes/signals.py
@@ -1,11 +1,11 @@
+""" This module contains signal receivers for the recipes app. """
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import Profile
 
-# Signal receiver to create a user profile when a new user is created
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
+    """ Creates a Profile object for a new User object. """
     if created:
-        # If the user instance is newly created, create a corresponding Profile instance
         Profile.objects.create(user=instance)
